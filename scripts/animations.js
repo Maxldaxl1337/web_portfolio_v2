42ccc9870b06a510316d6758813f3afe459e9564
Maximilian <93908181+Maxldaxl1337@users.noreply.github.com>
2024-05-28 11:15:02 +0200

    added title for hero

---
+++ scripts/animations.js
@@ -0,0 +1,39 @@
+// TYPE WRITE //
+window.onload = function () {
+    typeWriter('typewrite1', text1, 60, function () {
+      typeWriter('typewrite2', text2, 60);
+    });
+  };
+
+  const text1 = "Maximilian";
+  const text2 = " Huetter";
+
+  function typeWriter(elementId, text, speed, callback) {
+    const element = document.getElementById(elementId);
+    let i = 0;
+
+    function type() {
+      if (i < text.length) {
+        element.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(type, speed);
+      } else {
+        if (callback) {
+          callback(); 
+        }
+      }
+    }
+
+    type();
+  }
+
+//BLINKY BLINKY//
+let visibleCursor = false
+setInterval(() => {
+    if (visibleCursor){
+        document.getElementById("cursor").style.opacity = 0;
+    } else {
+        document.getElementById("cursor").style.opacity = 1;
+    }
+    visibleCursor = !visibleCursor
+}, 400);
