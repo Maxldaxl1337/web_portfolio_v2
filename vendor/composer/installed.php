ff445ae161fe188f3b6076bf85409148cb7e76d0
Maximilian <93908181+Maxldaxl1337@users.noreply.github.com>
2025-01-20 20:37:08 +0100

    task (composer): add dependency manager
    Tested Brew and installed php to test some stuff with Fluid (will redo this)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e5c8bdb742538118a9d3375c17760db74d966eeb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e5c8bdb742538118a9d3375c17760db74d966eeb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'typo3fluid/fluid' => array(
+            'pretty_version' => '4.0.2',
+            'version' => '4.0.2.0',
+            'reference' => '5ecd828cfeb52c6d4583f48bb5c477346b62903c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../typo3fluid/fluid',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
